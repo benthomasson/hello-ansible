1b7d46086e1ba3a2eae96735c64ce6051d169c6e
Ben Thomasson <bthomass@redhat.com>
2024-01-05 14:16:06 -0500

    Update say for new action plugin interface

--- extensions/eda/plugins/rule_action/say.py
+++ extensions/eda/plugins/rule_action/say.py
@@ -1,42 +1,15 @@
 import subprocess
-from typing import List, Dict
-import uuid
-from ansible_rulebook.util import run_at
-from ansible_rulebook.builtin import _get_events
 
-SUCCESSFUL = "successful"
+from ansible_rulebook.action.control import Control
+from ansible_rulebook.action.helper import Helper
+from ansible_rulebook.action.metadata import Metadata
 
 
-async def main(
-    event_log,
-    inventory: str,
-    hosts: List,
-    variables: Dict,
-    project_data_file: str,
-    source_ruleset_name: str,
-    source_ruleset_uuid: str,
-    source_rule_name: str,
-    source_rule_uuid: str,
-    rule_run_at: str,
-    text: str,
-):
+async def main(metadata: Metadata, control: Control, **action_args) -> None:
+    helper = Helper(metadata, control, "say")
+    text = action_args.get("text", None)
     try:
         subprocess.run(["say", "--", text])
     except KeyboardInterrupt:
         pass
-    await event_log.put(
-        dict(
-            type="Action",
-            action="say",
-            action_uuid=str(uuid.uuid4()),
-            ruleset=source_ruleset_name,
-            ruleset_uuid=source_ruleset_uuid,
-            rule=source_rule_name,
-            rule_uuid=source_rule_uuid,
-            activation_id="???",
-            run_at=run_at(),
-            status=SUCCESSFUL,
-            matching_events=_get_events(variables),
-            rule_run_at=rule_run_at,
-        )
-    )
+    await helper.send_default_status()
